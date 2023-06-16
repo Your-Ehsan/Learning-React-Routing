733598add1341b98da9c44b003d7df53c9cc83c6
your-ehsan <ehsanshahid787@gmail.com>
2023-06-16 17:48:11 +0500

    lesson one completed with react routing :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// write App function in react and add routing in it with BrowserRouter, Link, Route and Routes
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/contact"}>Contact us</Link>
+        <Link to={"/about"}>About us</Link>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
-
-export default App
+function Home() {
+  return (
+    <>
+      <h1>this is Home section !</h1>
+    </>
+  );
+}
+function Contact() {
+  return (
+    <>
+      <h1>this is Contact section !</h1>
+    </>
+  );
+}
+function About() {
+  return (
+    <>
+      <h1>this is About section !</h1>
+    </>
+  );
+}
+export default App;
