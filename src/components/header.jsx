6052d54ea107fc2eedffc7a92be2a957f5d96abd
EHSAN <93027825+Your-Ehsan@users.noreply.github.com>
2023-06-21 16:53:30 +0500

    navLink concept added | react routing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,22 +1,38 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function Header() {
+  const NavClasses = "mx-2 leading-snug text-slate-200 mx-4 text-white ";
   return (
     <>
-      <Link className="mx-2 leading-snug text-slate-200 font-medium" to={"/"}>
+      <NavLink
+        className={(e) =>
+          e.isActive
+            ? `${NavClasses}` + "font-semibold underline"
+            : `${NavClasses}`
+        }
+        to={"/"}
+      >
         Home
-      </Link>
-      <Link
-        className="mx-2 leading-snug text-slate-200 font-medium"
+      </NavLink>
+      <NavLink
+        className={(e) =>
+          e.isActive
+            ? `${NavClasses}` + "font-semibold underline"
+            : `${NavClasses}`
+        }
         to={"/contact"}
       >
         Contact us
-      </Link>
-      <Link
-        className="mx-2 leading-snug text-slate-200 font-medium"
+      </NavLink>
+      <NavLink
+        className={(e) =>
+          e.isActive
+            ? `${NavClasses}` + "font-semibold underline"
+            : `${NavClasses}`
+        }
         to={"/about"}
       >
         About us
-      </Link>
+      </NavLink>
     </>
   );
 }
